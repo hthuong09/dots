137fb4a9a8e961e384df8e90650f16933b6050c1
Thương Nguyễn <thuongnguyen.net@gmail.com>
2019-03-02 01:55:07 +0700

    update gpg key

--- .zsh/gpg/init.zsh
+++ .zsh/gpg/init.zsh
@@ -1,5 +1,5 @@
-alias sendgpgkey="gpg --keyserver pool.sks-keyservers.net --send-keys 7CFC941A76EDB5FD"
-alias receivegpgkey="gpg --keyserver pool.sks-keyservers.net --recv-keys 7CFC941A76EDB5FD"
+alias sendgpgkey="gpg --keyserver pool.sks-keyservers.net --send-keys E01DB409642851D1"
+alias receivegpgkey="gpg --keyserver pool.sks-keyservers.net --recv-keys E01DB409642851D1"
 
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
