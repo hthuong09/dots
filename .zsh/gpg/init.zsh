d768ca16872d0e8b331c8be4d64fe3a2dfb0d3de
Thương Nguyễn <thuongnguyen.net@gmail.com>
2019-03-02 01:16:50 +0700

    alias for send a receive gpg key

--- .zsh/gpg/init.zsh
+++ .zsh/gpg/init.zsh
@@ -1,3 +1,7 @@
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
 gpg-connect-agent updatestartuptty /bye > /dev/null
+
+
+alias sendgpgkey "gpg --keyserver pool.sks-keyservers.net --send-keys 7CFC941A76EDB5FD"
+alias receivegpgkey "gpg --keyserver pool.sks-keyservers.net --recv-keys 7CFC941A76EDB5FD"
