6e0df0c0dbd8a33a967ae9fab229fc97c7fac6a0
Thuong Nguyen <thuongnguyen.net@gmail.com>
2019-03-02 01:32:14 +0700

    add: gitmodules for zsh external library

--- .zsh/gpg/init.zsh
+++ .zsh/gpg/init.zsh
@@ -1,7 +1,7 @@
+alias sendgpgkey="gpg --keyserver pool.sks-keyservers.net --send-keys 7CFC941A76EDB5FD"
+alias receivegpgkey="gpg --keyserver pool.sks-keyservers.net --recv-keys 7CFC941A76EDB5FD"
+
 export GPG_TTY="$(tty)"
 export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
 gpg-connect-agent updatestartuptty /bye > /dev/null
 
-
-alias sendgpgkey "gpg --keyserver pool.sks-keyservers.net --send-keys 7CFC941A76EDB5FD"
-alias receivegpgkey "gpg --keyserver pool.sks-keyservers.net --recv-keys 7CFC941A76EDB5FD"
