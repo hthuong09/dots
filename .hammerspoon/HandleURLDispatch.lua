fcb1668248f45b6eb0dea2dea1cbbc09cd3d973b
Thuong Nguyen <thuongnguyen.net@gmail.com>
2022-09-02 19:13:53 +0700

    update

--- .hammerspoon/HandleURLDispatch.lua
+++ .hammerspoon/HandleURLDispatch.lua
@@ -1,48 +1,49 @@
 function module(logger)
-  local workProfile = "Profile 1"
-  local personalProfile = "Profile 2"
+	local workProfile = "Profile 1"
+	local personalProfile = "Profile 2"
 
+	hs.loadSpoon("URLDispatcher")
 
-  hs.loadSpoon("URLDispatcher")
+	local openLinkWithChromePorifle = function(url, profile)
+		local t = hs.task.new("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", nil, function()
+			return false
+		end, { "--profile-directory=" .. profile, url })
+		t:start()
+	end
 
-  local openLinkWithChromePorifle = function(url, profile)
-    local t = hs.task.new(
-        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-        nil,
-        function() return false end,
-        {"--profile-directory=" .. profile, url}
-    )
-    t:start()
-  end
+	local sendToProfile = function(t)
+		local fn = function(url)
+			openLinkWithChromePorifle(url, t[2])
+		end
+		return { t[1], nil, fn }
+	end
 
-  local sendToProfile = function(t)
-      local fn = function(url)
-        openLinkWithChromePorifle(url, t[2])
-      end
-      return {t[1], nil, fn}
-  end
+	spoon.URLDispatcher.url_patterns = {
+		sendToProfile({ "https?://shopadmin%.atlassian%.net", workProfile }),
+		sendToProfile({ ".*amplitude%.com", workProfile }),
+		sendToProfile({ "https?://github%.com/shopback/", workProfile }),
+		sendToProfile({ "https?://gitlab%.com/shopback/", workProfile }),
+		sendToProfile({ "https?://device%.sso%.ap%-southeast%-1%.amazonaws%.com/", workProfile }),
+		{
+			".*",
+			nil,
+			function(url, senderPID)
+				local triggerApp = hs.application.applicationForPID(senderPID)
+				if triggerApp then
+					if triggerApp:name() == "Slack" then
+						openLinkWithChromePorifle(url, workProfile)
+						return
+					end
+				end
+				openLinkWithChromePorifle(url, personalProfile)
+			end,
+		},
+		sendToProfile({ ".*", personalProfile }),
+	}
 
-  spoon.URLDispatcher.url_patterns = {
-      sendToProfile{"https?://shopadmin%.atlassian%.net", workProfile},
-      sendToProfile{".*amplitude%.com", workProfile},
-      sendToProfile{"https?://github%.com/shopback/", workProfile},
-      sendToProfile{"https?://device%.sso%.ap%-southeast%-1%.amazonaws%.com/", workProfile},
-      {".*", nil, function(url, senderPID)
-        local triggerApp = hs.application.applicationForPID(senderPID)
-        if triggerApp then
-          if (triggerApp:name() == "Slack") then
-            openLinkWithChromePorifle(url, workProfile)
-            return
-          end
-        end
-        openLinkWithChromePorifle(url, personalProfile)
-      end},
-      sendToProfile{".*", personalProfile},
-  }
+	spoon.URLDispatcher:start()
 
-  spoon.URLDispatcher:start()
-
-  logger:i('[HandleUrlDispatch] module loaded')
+	logger:i("[HandleUrlDispatch] module loaded")
 end
 
 return module
