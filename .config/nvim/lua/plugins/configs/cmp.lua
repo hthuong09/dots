049cb4d26dad89a527dc4f5fad3e87a593e0c4c3
Thuong Nguyen <thuongnguyen.net@gmail.com>
2023-02-28 21:36:40 +0700

    update

--- .config/nvim/lua/plugins/configs/cmp.lua
+++ .config/nvim/lua/plugins/configs/cmp.lua
@@ -7,8 +7,6 @@ function M.config()
 		return
 	end
 
-	vim.opt.completeopt = "menuone,noselect"
-
 	local function has_words_before()
 		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
@@ -36,6 +34,10 @@ function M.config()
 	end
 
 	local options = {
+		completion = {
+			-- auto select first item
+			completeopt = "menu,menuone,noinsert",
+		},
 		window = {
 			completion = {
 				border = border("CmpBorder"),
