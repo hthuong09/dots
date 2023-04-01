0a167dc5b124b43c119255774c7895afe411f115
Thuong Nguyen <thuongnguyen.net@gmail.com>
2023-04-01 23:58:48 +0700

    update

--- .config/nvim/lua/plugins/configs/base46.lua
+++ .config/nvim/lua/plugins/configs/base46.lua
@@ -11,7 +11,7 @@ function M.config()
 		hl_override = {
 			DiffAdd = { fg = "#a4b595" },
 			DiffChange = { fg = "#DE935F" },
-			NormalFloat = { bg = "NONE" },
+			-- NormalFloat = { bg = "NONE" },
 
 			-- AlphaButtons = { fg = "#81A1C1" },
 			Normal = {
