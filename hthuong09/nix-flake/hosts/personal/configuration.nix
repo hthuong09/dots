1ed65e8079a65d22ffc46151b67b23fde92c9a95
Thuong Nguyen <thuongnguyen.net@gmail.com>
2025-01-03 09:49:10 +0700

    update

--- hthuong09/nix-flake/hosts/personal/configuration.nix
+++ hthuong09/nix-flake/hosts/personal/configuration.nix
@@ -4,5 +4,9 @@
   imports = [ ../../base/configuration.nix ];
 
   environment.systemPackages = [ pkgs.nixfmt-rfc-style ];
-  homebrew.casks = [ "ghostty" ];
+  homebrew.casks = [
+    "ghostty"
+    "1password"
+    "1password-cli"
+  ];
 }
